3121ceb55c195c406ded7ca8b3d7e304ac0747cc
monarch-one <github@micorreo.uk>
2025-07-05 19:46:59 -0300

    🛠️ Agregar archivos de favoritos (JSON + HTML)

--- main.go
+++ main.go
@@ -20,6 +20,12 @@ type Article struct {
 	Date  string
 }
 
+type FavoriteArticle struct {
+	Title string `json:"title"`
+	Link  string `json:"link"`
+	Date  string `json:"date"`
+}
+
 var mu sync.Mutex
 
 func loadFeeds() []Feed {
@@ -38,6 +44,27 @@ func saveFeed(newFeed Feed) {
 
 	data, _ := json.MarshalIndent(feeds, "", "  ")
 	os.WriteFile("feeds.json", data, 0644)
+}
+
+func saveFavorite(article FavoriteArticle) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	var favorites []FavoriteArticle
+	data, _ := os.ReadFile("favorites.json")
+	json.Unmarshal(data, &favorites)
+
+	// Evitar duplicados
+	for _, fav := range favorites {
+		if fav.Link == article.Link {
+			return
+		}
+	}
+
+	favorites = append(favorites, article)
+
+	updated, _ := json.MarshalIndent(favorites, "", "  ")
+	os.WriteFile("favorites.json", updated, 0644)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -78,19 +105,45 @@ func addHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func favoriteHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	link := r.FormValue("link")
+	date := r.FormValue("date")
+
+	if title != "" && link != "" {
+		fav := FavoriteArticle{
+			Title: title,
+			Link:  link,
+			Date:  date,
+		}
+		saveFavorite(fav)
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func favoritesPage(w http.ResponseWriter, r *http.Request) {
+	data, _ := os.ReadFile("favorites.json")
+	var favorites []FavoriteArticle
+	json.Unmarshal(data, &favorites)
+
+	tmpl := template.Must(template.ParseFiles("templates/favorites.html"))
+	tmpl.Execute(w, favorites)
+}
+
 func main() {
-	// Servir archivos estáticos como CSS
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/add", addHandler)
+	http.HandleFunc("/favorite", favoriteHandler)
+	http.HandleFunc("/favorites", favoritesPage)
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080" // fallback local
+		port = "8080"
 	}
 	http.ListenAndServe(":"+port, nil)
-
 }
